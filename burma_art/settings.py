8bf0ed73ff0068ef45ff8e8ba105156fa9cb1f6f
Phyo Naing <phyonaing11@gmail.com>
2025-02-06 13:58:10 +0630

    test

--- burma_art/settings.py
+++ burma_art/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = 'django-insecure-dor1m%-7ljuza%6^9f=bj)+i$(%d-j%780-=rthn(all!ldv#8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["burmaart-production.up.railway.app","https://burmaart-production.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ["burmaart-production.up.railway.app","https://burmaart-production.up.railway.app"]
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
 
 # Application definition
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'burma_art.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         #'ENGINE': 'django.db.backends.sqlite3',
-#         #'NAME': BASE_DIR / 'db.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': os.getenv('DB_PASSWORD_YO'),
-#         'HOST': os.getenv('DATABASE_URL'),
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD_YO'),
+        'HOST': os.getenv('DATABASE_URL'),
+        'PORT': '5432',
+    }
+}
 
 DATABASES = {
     'default': dj_database_url.config(

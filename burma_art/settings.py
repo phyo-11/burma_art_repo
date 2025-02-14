b8d0a4abdcde580c6b58b1fa10377cf4ba727660
phyo-11 <phyonaing11@gmail.com>
2025-02-14 10:40:52 +0630

    fixed procfile

--- burma_art/settings.py
+++ burma_art/settings.py
@@ -31,8 +31,17 @@ SECRET_KEY = 'django-insecure-dor1m%-7ljuza%6^9f=bj)+i$(%d-j%780-=rthn(all!ldv#8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://burmaart.art', 'burmaart.art', 'burmaartrepo-production.up.railway.app','https://burmaartrepo-production.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://burmaart.art', 'burmaart.art', 'https://burmaartrepo-production.up.railway.app']
+ALLOWED_HOSTS = [
+    "burmaart.art",
+    "burmaartrepo-production.up.railway.app",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://burmaart.art",
+    "https://burmaartrepo-production.up.railway.app",
+]
+
 
 
 # Application definition
